5e037de635172528fbfe967a971e8b0810dabc7c
Luiggi <luduve97@gmail.com>
2023-05-22 12:59:24 +0200

    useeffectpractice

--- src/App.tsx
+++ src/App.tsx
@@ -1,34 +1,23 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
+import { useState, useEffect } from 'react'
 import './App.css'
+import Button from './Button';
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [itemChange, setItemChange] = useState<string>('mystate');
+
+  useEffect( () => {
+    console.log("el state actual es", itemChange);
+    }
+  )
 
   return (
-    <>
-      <div>
-        <a href="https://vitejs.dev" target="_blank">
-          <img src={viteLogo} className="logo" alt="Vite logo" />
-        </a>
-        <a href="https://react.dev" target="_blank">
-          <img src={reactLogo} className="logo react" alt="React logo" />
-        </a>
-      </div>
-      <h1>Vite + React</h1>
-      <div className="card">
-        <button onClick={() => setCount((count) => count + 1)}>
-          count is {count}
-        </button>
-        <p>
-          Edit <code>src/App.tsx</code> and save to test HMR
-        </p>
-      </div>
-      <p className="read-the-docs">
-        Click on the Vite and React logos to learn more
-      </p>
-    </>
+    <div className="App">
+      <Button onClick={() => setItemChange("Comments")} text="Comments"/>
+      <Button onClick={() => setItemChange("Posts")} text="Posts"/>
+      <Button onClick={() => setItemChange("Stories")} text="Stories"/>
+
+      <div>{itemChange}</div>
+    </div>
   )
 }
 
