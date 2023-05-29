e1da912096bd60939a6aae46fa306a75caa639c6
Luiggi <luduve97@gmail.com>
2023-05-29 20:36:28 +0200

    added buttons ex 3

--- src/App.tsx
+++ src/App.tsx
@@ -8,115 +8,134 @@ import { CheckboxContainer, WebsiteContainer } from "./utils";
 // https://www.youtube.com/watch?v=2aumoR0-jmQ
 
 function App() {
-	const [total, setTotal] = useState<number>(0);
-	// state para precio final de los checkeds
-	const [isCheckedState, setIsCheckedState] = useState<boolean[]>(
-		new Array(checkboxData.length).fill(false)
-	);
-	const [totalPages, setTotalPages] = useState<number>(1);
-	const [totalLanguages, setTotalLanguages] = useState<number>(1);
-	const [totalExtras, setTotalExtras] = useState<number>(0);
+  const [total, setTotal] = useState<number>(0);
+  const [isCheckedState, setIsCheckedState] = useState<boolean[]>(
+    new Array(checkboxData.length).fill(false)
+  );
+  const [totalPages, setTotalPages] = useState<number>(0);
+  const [totalLanguages, setTotalLanguages] = useState<number>(0);
 
-	const handlerOnChange = (itemPosition: number) => {
-		const updateCheckedState = isCheckedState.map(
-			(item, index) => (index === itemPosition ? !item : item)
-			//esto marca la casilla. si el indice es identico a la posicion entonces se devuelve el valor contrario del item, que es true;
-			//tener en cuenta que no podemos poner "true" en el ternario. O sino siempre sera true, siendo imposible uncheckearlo
-		);
+  const handlerOnChange = (itemPosition: number) => {
+    const updateCheckedState = isCheckedState.map(
+      (item, index) => (index === itemPosition ? !item : item)
+      //esto marca la casilla. si el indice es identico a la posicion entonces se devuelve el valor contrario del item, que es true;
+      //tener en cuenta que no podemos poner "true" en el ternario. O sino siempre sera true, siendo imposible uncheckearlo
+    );
+    setIsCheckedState(updateCheckedState);
+  };
 
-		setIsCheckedState(updateCheckedState);
+  const totalWebsite = (pages: number, languages: number) => {
+    return pages * languages * 30;
+  };
 
-		const totalPrice = updateCheckedState.reduce(
-			(sumaTotal, currentState, index) => {
-				if (currentState === true) {
-					return sumaTotal + checkboxData[index].price;
-				}
-				return sumaTotal;
-			},
-			0
-		);
+  useEffect(() => {
+    let totalPrice = 0;
+    //inicializamos fuera del if
+    isCheckedState.forEach((checked, index) => {
+      if (checked) {
+        totalPrice += checkboxData[index].price;
+      }
+    });
 
-		const extraPrice = totalWebsite(totalPages, totalLanguages);
-		//funciona correctamente
-		console.log("extraprice", extraPrice);
+    const extraPrice = totalWebsite(totalPages, totalLanguages);
+    const globalTotal = totalPrice + extraPrice;
+    setTotal(globalTotal);
+  }, [isCheckedState, totalPages, totalLanguages]);
 
-		const globalTotal = extraPrice + totalPrice;
-		console.log("globalTotal: extraPrice + totalPrice", globalTotal);
+  const onPageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const pages = Number(e.target.value);
+    setTotalPages(pages);
+  };
+  const onLanguageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
+    const languages = Number(e.target.value);
+    setTotalLanguages(languages);
+  };
 
-		setTotal(globalTotal);
-		// console.log("setTotalpages", totalPages);
-	};
+  return (
+    <>
+      <CheckboxContainer>
+        <h1>Qué quieres hacer?</h1>
+        {checkboxData.map((item, index) => (
+          <React.Fragment key={index}>
+            <Checkbox
+              text={item.text}
+              price={item.price}
+              isChecked={isCheckedState[index]}
+              onChange={() => handlerOnChange(index)}
+              id={item.id}
+            />
 
-	const totalWebsite = (pages: number, languages: number) => {
-		return pages * languages * 30;
-	};
-// LAS CASILLAS DE LAS PAGINAS NO SE REDUCIRAN PORQUE EL ONCHANGE OCURRE FUERA DE LAS CHECKBOX. 
-	useEffect(() => {
-		const extraPrice = totalWebsite(totalPages, totalLanguages);
-		console.log("extraprice", extraPrice);
-		setTotalExtras(extraPrice);
-		setTotal(total + totalExtras);
+            {item.id === "website" && isCheckedState[index] && (
+              <WebsiteContainer className="scale-in-ver-center">
+                <div>
+                  <p>Número de páginas</p>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      if (totalPages > 1) {
+                        setTotalPages(totalPages - 1);
+                      }
+                    }}
+                  >
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    value={totalPages}
+                    onChange={onPageChange}
+                    min="1"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => {
+                      if (totalPages >= 0) {
+                        setTotalPages(totalPages + 1);
+                      }
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
+                <div>
+                  <p>Número de idiomas</p>
+                  <button
+                    type="button"
+                    onClick={() => {
+                      if (totalLanguages > 1) {
+                        setTotalLanguages(totalLanguages - 1);
+                      }
+                    }}
+                  >
+                    -
+                  </button>
+                  <input
+                    type="number"
+                    value={totalLanguages}
+                    onChange={onLanguageChange}
+                    min="1"
+                  />
+                  <button
+                    type="button"
+                    onClick={() => {
+                      if (totalLanguages >= 0) {
+                        setTotalLanguages(totalLanguages + 1);
+                      }
+                    }}
+                  >
+                    +
+                  </button>
+                </div>
+              </WebsiteContainer>
+            )}
+          </React.Fragment>
+        ))}
+      </CheckboxContainer>
 
-		console.log(
-			"total after setting globaltotal in setTtoal in useEffect",
-			total
-		);
-	}, [totalPages, totalLanguages]);
+      <h3>Total Price: ${total}</h3>
 
-	const onPageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const pages = Number(e.target.value);
-		setTotalPages(pages);
-	};
-	const onLanguageChange = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const languages = Number(e.target.value);
-		setTotalLanguages(languages);
-	};
-
-	return (
-		<>
-			<CheckboxContainer>
-				<h1>Qué quieres hacer?</h1>
-				{checkboxData.map((item, index) => (
-					<React.Fragment key={index}>
-						<Checkbox
-							text={item.text}
-							price={item.price}
-							isChecked={isCheckedState[index]}
-							onChange={() => handlerOnChange(index)}
-							id={item.id}
-						/>
-
-						{item.id === "website" && isCheckedState[index] && (
-							<WebsiteContainer className="scale-in-ver-center">
-								<div>
-									<p>Número de páginas</p>
-									<input
-										type="text"
-										value={totalPages}
-										onChange={onPageChange}
-										min="1"
-									/>
-								</div>
-								<div>
-									<p>Número de idiomas</p>
-									<input
-										type="text"
-										value={totalLanguages}
-										onChange={onLanguageChange}
-										min="1"
-									/>
-								</div>
-							</WebsiteContainer>
-						)}
-					</React.Fragment>
-				))}
-			</CheckboxContainer>
-
-			<h3>Total Price: ${total}</h3>
-
-			{/*!!!importante https://www.freecodecamp.org/news/how-to-work-with-multiple-checkboxes-in-react/ */}
-		</>
-	);
+      {/*!!!importante https://www.freecodecamp.org/news/how-to-work-with-multiple-checkboxes-in-react/ */}
+    </>
+  );
 }
 
 export default App;
