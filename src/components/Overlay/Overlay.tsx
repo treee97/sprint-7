77e1b2631aa35c85a5053205223e5d949e67cc88
Luiggi <luduve97@gmail.com>
2023-05-31 19:44:45 +0200

    modal is completed kinda

--- src/components/Overlay/Overlay.tsx
+++ src/components/Overlay/Overlay.tsx
@@ -1,15 +1,17 @@
-import React from "react";
 import { OverlayContainer, OverlayItem } from "../../utils";
 // TfiHelpAlt;
+type overlayProp = {
+  onClick: () => void;
+};
 
-const Overlay = () => {
-	return (
-		<OverlayContainer>
-			<OverlayItem>
-				Hello im IN the OverlayContainer. Im the overlay ITEM
-			</OverlayItem>
-		</OverlayContainer>
-	);
+const Overlay = ({ onClick }: overlayProp) => {
+  return (
+    <OverlayContainer onClick={onClick}>
+      <OverlayItem>
+        Hello im IN the OverlayContainer. Im the overlay ITEM
+      </OverlayItem>
+    </OverlayContainer>
+  );
 };
 
 export default Overlay;
