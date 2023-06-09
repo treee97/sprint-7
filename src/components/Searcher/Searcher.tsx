98682ca68846b6cb1350887dd8254b733576fd53
Luiggi <luduve97@gmail.com>
2023-06-09 12:45:00 +0200

    2 filters working

--- src/components/Searcher/Searcher.tsx
+++ src/components/Searcher/Searcher.tsx
@@ -2,24 +2,25 @@ import { AiOutlineSearch } from "react-icons/ai";
 import { useState } from "react";
 import { dataType } from "../../utils/types/types";
 
-const Searcher = (data: dataType) => {
-	const [search, setSearch] = useState<string>("");
+const Searcher = ({ data }: { data: dataType[] }) => {
+  const [search, setSearch] = useState<string>("");
 
-	const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
-		const matchItem = data.filter((item) => {});
-	};
-	return (
-		<>
-			<AiOutlineSearch />
-			<input
-				type="text"
-				name="search"
-				value={search}
-				onChange={handleSearch}
-				id="search"
-			/>
-		</>
-	);
+  const handleSearch = (e: React.ChangeEvent<HTMLInputElement>) => {
+    setSearch(e.target.value);
+  };
+
+  return (
+    <>
+      <AiOutlineSearch />
+      <input
+        type="text"
+        name="search"
+        value={search}
+        onChange={handleSearch}
+        id="search"
+      />
+    </>
+  );
 };
 
 export default Searcher;
--- src/utils/SortFilters/SortFilters.tsx
+++ src/utils/SortFilters/SortFilters.tsx
@@ -1,47 +1,46 @@
 import { dataType } from "../types/types";
+export const SortAlphabetically = (data: dataType[], sorted: boolean) => {
+  const arr = [...data];
+  if (sorted) {
+    const sortedArr = arr.sort((itemA, itemB) =>
+      itemA.clienteNombre.localeCompare(itemB.clienteNombre)
+    );
+    console.log(sorted);
+    console.log("a compara itemBBB", sortedArr);
+    return sortedArr;
+  } else {
+    const sortedArr = arr.sort((itemA, itemB) =>
+      itemB.clienteNombre.localeCompare(itemA.clienteNombre)
+    );
 
-export const SortAlphabetically = (data: dataType[]) => {
-	let sorted = true;
-	const arr = [...data];
-	if (sorted) {
-		const sortedArr = arr.sort((itemA, itemB) =>
-			itemA.clienteNombre.localeCompare(itemB.clienteNombre)
-		);
-		sorted = false;
-		console.log(sorted);
-		console.log("a compara itemBBB", sortedArr);
-
-		return sortedArr;
-	}
-	if (sorted === false) {
-		sorted = true;
-		const sortedArr = arr.sort((itemA, itemB) =>
-			itemB.clienteNombre.localeCompare(itemA.clienteNombre)
-		);
-
-		console.log("b, compara itema", sortedArr);
-		return sortedArr;
-	}
+    console.log("b, compara itema", sortedArr);
+    return sortedArr;
+  }
 };
 
-export const SortByDate = (data: dataType[]) => {
-	const arr = [...data];
-	const sortByDate = arr.sort((a, b) =>
-		a.date
-			.split("/")
-			.reverse()
-			.join()
-			.localeCompare(b.date.split("/").reverse().join())
-	);
-	console.log(sortByDate);
+export const SortByDate = (data: dataType[], sorted: boolean) => {
+  const arr = [...data];
+  if (sorted) {
+    const sortByDate = arr.sort((a, b) =>
+      a.date.split("/").join().localeCompare(b.date.split("/").join())
+    );
+    // console.log("acompareBB", sortByDate);
 
-	return sortByDate;
+    return sortByDate;
+  } else {
+    const sortByDate = arr.sort((a, b) =>
+      b.date.split("/").join().localeCompare(a.date.split("/").join())
+    );
+    // console.log("bcompareAA", sortByDate);
+
+    return sortByDate;
+  }
 };
 
 export const ReverseSort = (data: dataType[]) => {
-	const arr = [...data];
-	console.log("arr", arr);
-	//wtf
+  const arr = [...data];
+  console.log("arr", arr);
+  //wtf
 
-	return arr;
+  return arr;
 };
